7d28364dc18ef1bd4b0f171cb1b2c1dbd2d6430f
Ryo Fukumuro <rkworks@o0o.co>
2014-11-13 23:15:54 +0900

    [WIP] refactoring

--- src/clj_paginator/pagination.clj
+++ src/clj_paginator/pagination.clj
@@ -29,14 +29,20 @@
 (defn per-page [pagination]
   (get pagination :per-page))
 
-(defn total-items [pagination]
-  (get pagination :total-items))
+(defn total-entries [pagination]
+  @(get pagination :total-entries))
+
+(defn entries [pagination]
+  @(get pagination :entries))
 
 (defn window-size [pagination]
   (get pagination :window-size))
 
+(defn total-pages* [pagination]
+  (int (Math/ceil (/ (total-entries pagination) (per-page pagination)))))
+
 (defn total-pages [pagination]
-  (int (Math/ceil (/ (total-items pagination) (per-page pagination)))))
+  @(get pagination :total-pages))
 
 (defn next-page [pagination]
   (let [page (page pagination)]
@@ -46,31 +52,45 @@
   (let [page (page pagination)]
     (when (> page 1) (dec page))))
 
-(defn pages-in-window* [page last-page window-size]
-  (let [st     (- page window-size)
-        ed     (+ page window-size)
-        offset (max 0 (- 1 st))
-        st     (max 1 st)
-        ed     (+ ed offset)
-        offset (max 0 (- ed last-page))
-        ed     (min last-page ed)
-        st     (max 1 (- st offset))]
-    (range st (inc ed))))
+(defn pages-in-window
+  ([pagination]
+     (pages-in-window (page pagination)
+                      (total-pages pagination)
+                      (window-size pagination)))
+  ([page last-page window-size]
+     (let [st     (- page window-size)
+           ed     (+ page window-size)
+           offset (max 0 (- 1 st))
+           st     (max 1 st)
+           ed     (+ ed offset)
+           offset (max 0 (- ed last-page))
+           ed     (min last-page ed)
+           st     (max 1 (- st offset))]
+       (range st (inc ed)))))
 
-(defn pages-in-window [pagination]
-  (pages-in-window* (page pagination)
-                    (total-pages pagination)
-                    (window-size pagination)))
+;; (defn paginate [req pageable & [{:keys [page per-page window-size]}]]
+;;   (let [page        (or page (get-current-page-from-request req) 1)
+;;         per-page    (or per-page 20)
+;;         total-entries (count-all pageable)
+;;         entries       (find-all pageable page per-page)
+;;         window-size (or window-size 2)]
+;;     {:page            page
+;;      :window-size     window-size
+;;      :per-page        per-page
+;;      :total-entries   total-entries
+;;      :entries         entries
+;;      :route-generator (partial gen-route req)}))
 
 (defn paginate [req pageable & [{:keys [page per-page window-size]}]]
   (let [page        (or page (get-current-page-from-request req) 1)
         per-page    (or per-page 20)
-        total-items (count-all pageable)
-        items       (find-all pageable page per-page)
-        window-size (or window-size 2)]
-    {:page            page
-     :window-size     window-size
-     :per-page        per-page
-     :total-items     total-items
-     :items           items
-     :route-generator (partial gen-route req)}))
+        window-size (or window-size 2)
+        entries     (find-all pageable page per-page)
+        pagination  {:page            page
+                     :window-size     window-size
+                     :per-page        per-page
+                     :total-entries   (future (count-all pageable))
+                     :entries         (future (find-all pageable page per-page))
+                     :route-generator (partial gen-route req)}
+        pagination  (assoc pagination :total-pages (future (total-pages* pagination)))]
+    pagination))
